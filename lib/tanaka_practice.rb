3539700385ec60e2303bb9364fd575e308cb1535
a-rom <a1623at@aiit.ac.jp>
2016-08-09 09:40:55 +0000

    gemとtestのコーディング

--- lib/tanaka_practice.rb
+++ lib/tanaka_practice.rb
@@ -1,5 +1,55 @@
 require "tanaka_practice/version"
 
 module TanakaPractice
-  # Your code goes here...
+
+  class Main
+    def odd?(n)
+      if (n % 2) == 1
+        return true
+      else
+        return false
+      end
+    end
+
+    def even?(n)
+      return ! odd?(n)
+    end
+
+    def check_number?(n)
+      if(n >= 1000 && n<= 9999 && n % 2)
+         return true
+      else
+         return false
+      end
+    end
+
+    def enough_length?(t)
+      if(t.length >=3 && t.length <=8)
+        return true
+      else 
+        return false
+      end
+    end
+
+    def divide(n1,n2)
+      n3 = n2 / n1
+      return n3
+    end
+
+    def fizz_buzz(n)
+      if n % 3 == 0 && n % 5 == 0
+        return "FizzBuzz"
+      elsif n % 5 == 0
+        return "Buzz"
+      elsif n % 3 == 0
+        return "Fizz"
+      else
+        return ""
+      end
+    end
+   
+    def hello
+     puts "Hello"
+    end
+  end
 end
--- test/tanaka_practice_test.rb
+++ test/tanaka_practice_test.rb
@@ -1,11 +1,56 @@
 require 'test_helper'
 
 class TanakaPracticeTest < Minitest::Test
+  def setup
+    @main = ::TanakaPractice::Main.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::TanakaPractice::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_odd?
+    assert_equal false, @main.odd?(-10000), '10000 is not odd'
+    assert_equal false, @main.odd?(-2), '0 is not odd'
+    assert_equal true,  @main.odd?(-1), '0 is not odd'
+    assert_equal false, @main.odd?(0),  '0 is not odd'
+    assert_equal true,  @main.odd?(1),  '1 is odd'
+    assert_equal false, @main.odd?(2),  '2 is not odd'
+    assert_equal false, @main.odd?(10000), '10000 is not odd'
+  end
+
+  def test_even?
+    assert_equal true,  @main.even?(-10000), '10000 is even'
+    assert_equal true,  @main.even?(-2), '-2 is even'
+    assert_equal false, @main.even?(-1), '-1 is not even'
+    assert_equal true,  @main.even?(0),  '0 is even'
+    assert_equal false, @main.even?(1),  '1 is not even'
+    assert_equal true,  @main.even?(2),  '2 is even'
+    assert_equal true,  @main.even?(10000), '10000 is even'
+  end
+
+  def test_check_number?
+    assert_equal false,  @main.check_number?(10000)
+    assert_equal true,  @main.check_number?(1000)
+    assert_equal true,  @main.check_number?(5000)
+  end
+  
+
+  def divide
+    assert_equal 1,  @main.divide(100,100)
+    assert_equal 10,  @main.divide(10,100)
+    assert_equal 20,  @main.divide(5,100)
+    assert_equal 40,  @main.divide(5,200)
+  end
+
+  def enough_length?
+    assert_equal false,  @main.enough_length?("TANAKAAYUMU")
+    assert_equal true,   @main.enough_length?("A-TANAKA")
+    assert_equal true,  @main.enough_length?("TANA")
+    assert_equal false,  @main.enough_length?("TANAKATANAKATANAKA")
+  end
+  
+  def hello
+    assert_equal "Hello", @main.hello
   end
 end
